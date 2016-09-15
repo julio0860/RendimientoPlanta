29019d0161be83d440963347c7bbd9ed2d804d34
julio0860 <julio0860@hotmail.com>
2016-09-15 14:15:43 -0500

    Creacion de tablas para el agrupador

---
+++ app/src/main/java/com/adr/rendimientoplanta/DATA/T_Subproceso.java
@@ -0,0 +1,42 @@
+package com.adr.rendimientoplanta.DATA;
+
+/**
+ * Created by jcasas on 15/09/2016.
+ */
+public class T_Subproceso {
+
+    public static final String SUBID ="Sub_Id";
+    public static final String PROID = "Pro_Id";
+    public static final String SUBDESCRIPCION = "Sub_Descripcion";
+
+
+
+
+    public static final String N_TABLA = "Subproceso";
+
+    public static final String CREATE_T_SUBPROCESO ="CREATE TABLE " + N_TABLA+"("+
+            SUBID +" INTEGER PRIMARY KEY NOT NULL, "+
+            PROID +" INTEGER NOT NULL, "+
+            SUBDESCRIPCION +" TEXT NOT NULL, "+
+
+            ");";
+
+    public static final String DROP_T_SUBPROCESO ="DROP TABLE IF EXISTS "+N_TABLA;
+
+    public static String _INSERT(int ID,int PROID,String DESCRIPCION)
+
+    {
+        String _INSERT;
+        _INSERT = "INSERT INTO "+N_TABLA +"("+SUBID+","+PROID+","+SUBDESCRIPCION+","+
+                ")VALUES('"+
+                ID+"','"+PROID+"','"+DESCRIPCION+"');";
+        return _INSERT;
+    }
+
+    public static String _DELETE()
+    {
+        String _DELETE;
+        _DELETE = "DELETE FROM "+N_TABLA +";";
+        return _DELETE;
+    }
+}
