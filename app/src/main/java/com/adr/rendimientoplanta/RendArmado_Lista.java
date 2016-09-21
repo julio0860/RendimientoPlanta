efc414c23dd08c24cdb2d59ac1c2a4375d9590f2
julio0860 <julio0860@hotmail.com>
2016-09-21 13:20:01 -0500

    Actualizacion Lista Rendimiento

--- app/src/main/java/com/adr/rendimientoplanta/RendArmado_Lista.java
+++ app/src/main/java/com/adr/rendimientoplanta/RendArmado_Lista.java
@@ -62,13 +62,13 @@ public class RendArmado_Lista extends AppCompatActivity {
         lblLado.setText(Variables.Lin_Lado);
         edtFecha.setText(Variables.FechaStr);
 
-        Cursor Rse=LocBD.rawQuery(" SELECT M.POSICION AS '_id',IFNULL(A.DNI,' ') AS 'DNI' FROM MESA M lEFT JOIN  Agrupador A ON M.POSICION = A.Posicion AND A.Fecha='"+Variables.FechaStrBD+"' AND A.Suc_Id='"+Variables.Suc_Id+"'  AND  \n" +
-                "A.Pro_Id='"+Variables.Pro_Id+"'  AND A.Sub_Id='"+Variables.Sub_Id+"'  AND A.Lin_Id='"+Variables.Lin_Id+"' AND A.Lado='"+Variables.Lin_Lado+"'\n" +
+        Cursor Rse=LocBD.rawQuery(" SELECT M.POSICION AS '_id',IFNULL(A.DNI,' ') AS 'DNI',IFNULL(p.Per_ApePaterno+' '+p.Per_ApePaterno+' '+p.Per_Nombres,' ') AS 'PER'  FROM MESA M lEFT JOIN  Agrupador A ON M.POSICION = A.Posicion AND A.Fecha='"+Variables.FechaStrBD+"' AND A.Suc_Id='"+Variables.Suc_Id+"'  AND  \n" +
+                "A.Pro_Id='"+Variables.Pro_Id+"'  AND A.Sub_Id='"+Variables.Sub_Id+"'  AND A.Lin_Id='"+Variables.Lin_Id+"' AND A.Lado='"+Variables.Lin_Lado+"'  left join Personal p on p.Per_Codigo=A.DNI \n" +
                 " WHERE M.posicion <=\n" +
                 "(SELECT MESA FROM MesaLinea where Cam_Id=37 AND Pro_Id='"+Variables.Pro_Id+"'  AND Sub_Id='"+Variables.Sub_Id+"' AND Lin_Id='"+Variables.Lin_Id+"' AND Lado='"+Variables.Lin_Lado+"')",null);
 
-        AdaptadorGrilla = new SimpleCursorAdapter(RendArmado_Lista.this, android.R.layout.simple_list_item_2,Rse, new String[]{"_id","DNI"},
-               new int[]{android.R.id.text1,android.R.id.text2},SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        AdaptadorGrilla = new SimpleCursorAdapter(RendArmado_Lista.this, R.layout.simple_list_item_3,Rse, new String[]{"_id","DNI","PER"},
+               new int[]{R.id.text1,R.id.text2,R.id.text3},SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         dgvPersonalRendimiento.setAdapter(AdaptadorGrilla);
 
 
