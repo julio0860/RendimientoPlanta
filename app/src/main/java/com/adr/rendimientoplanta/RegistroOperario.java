a176852c529a0aa359af254bd3679a1a5e391ab6
julio0860 <julio0860@hotmail.com>
2016-09-22 07:52:44 -0500

    Actualizacion Lista Rendimiento

--- app/src/main/java/com/adr/rendimientoplanta/RegistroOperario.java
+++ app/src/main/java/com/adr/rendimientoplanta/RegistroOperario.java
@@ -1,10 +1,13 @@
 package com.adr.rendimientoplanta;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.adr.rendimientoplanta.DATA.LocalBD;
 import com.adr.rendimientoplanta.LIBRERIA.Variables;
 
 public class RegistroOperario extends AppCompatActivity {
@@ -15,10 +18,15 @@ public class RegistroOperario extends AppCompatActivity {
     private TextView lblLinea;
     private TextView lblLado;
     private EditText edtFecha;
+    private EditText edtDni;
+    private EditText edtPersonal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_operario);
+        LocalBD LBD = new LocalBD(RegistroOperario.this);
+
+        final SQLiteDatabase LocBD = LBD.getWritableDatabase();
 
         lblEmpresa = (TextView) findViewById(R.id.lblEmpresa);
         lblSucursal = (TextView) findViewById(R.id.lblSucursal);
@@ -27,6 +35,8 @@ public class RegistroOperario extends AppCompatActivity {
         lblLinea = (TextView) findViewById(R.id.lblLinea);
         lblLado = (TextView) findViewById(R.id.lblLado);
         edtFecha=(EditText) findViewById(R.id.edtFecha);
+        edtDni=(EditText)findViewById(R.id.edtDni);
+        edtPersonal=(EditText)findViewById(R.id.edtPersonal);
 
         //ASIGNACIÓN DE PARAMETROS A LA ACTIVIDAD
         lblEmpresa.setText(Variables.Emp_Abrev);
@@ -36,5 +46,14 @@ public class RegistroOperario extends AppCompatActivity {
         lblLinea.setText(Variables.Lin_Descripcion);
         lblLado.setText("LADO: "+Variables.Lin_Lado);
         edtFecha.setText(Variables.FechaStr);
+
+
+String Dni;
+
+        Dni=edtDni.getText().toString();
+
+        Cursor Rse=LocBD.rawQuery("SELECT Per_Codigo,Per_ApePaterno || ' '|| Per_ApeMaterno||' '||Per_Nombres AS 'PERSONAL' FROM PERSONAL WHERE Per_Codigo='"+Dni+"'",null);
+
+
     }
 }
