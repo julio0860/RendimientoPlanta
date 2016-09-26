542b3f07c9051148bc76b52f882cb6195ecbf0e0
julio0860 <julio0860@hotmail.com>
2016-09-26 13:50:42 -0500

    Actualizacion Lista Rendimiento

--- app/src/main/java/com/adr/rendimientoplanta/RegistroOperario.java
+++ app/src/main/java/com/adr/rendimientoplanta/RegistroOperario.java
@@ -250,6 +250,7 @@ public class RegistroOperario extends AppCompatActivity {
                               Intent ActividadRegresarLista = new Intent(RegistroOperario.this, RendArmado_Lista.class);
                               startActivity(ActividadRegresarLista);
 
+
                           } catch (Exception e) {
                               Estado=false;
                               Log.e(TAG, "Error Exception: " + e);
