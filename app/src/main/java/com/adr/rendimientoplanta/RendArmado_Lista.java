694055cf86997bafe553dcb978c280fde882e092
julio0860 <julio0860@hotmail.com>
2016-09-27 08:13:07 -0500

    Actualizacion Lista Rendimiento

--- app/src/main/java/com/adr/rendimientoplanta/RendArmado_Lista.java
+++ app/src/main/java/com/adr/rendimientoplanta/RendArmado_Lista.java
@@ -32,6 +32,7 @@ public class RendArmado_Lista extends AppCompatActivity {
     private ImageButton imbRegresar;
     private ImageButton imbConfigurar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +41,8 @@ public class RendArmado_Lista extends AppCompatActivity {
         final SQLiteDatabase LocBD = LBD.getWritableDatabase();
 
         AsignarVariables();
-        //ASIGNACIÓN DE PARAMETROS A LA ACTIVIDAD
-        lblEmpresa.setText(Variables.Emp_Abrev);
-        lblSucursal.setText(Variables.Suc_Descripcion);
-        lblLinea.setText(Variables.Lin_Descripcion);
-        lblLado.setText("LADO: " + Variables.Lin_Lado);
-        edtFecha.setText(Variables.FechaStr);
+        MostrarVariables();
+
 
         Cursor Rse = LocBD.rawQuery(" SELECT M.POSICION AS '_id',IFNULL(A.DNI,' ') AS 'DNI',IFNULL(p.Per_ApePaterno || ' '|| p.Per_ApeMaterno||' '||p.Per_Nombres,' ') AS 'PER',IFNULL(A.Agru_Id ,0) AS 'AGRUID',IFNULL(HoraIngreso,' ') AS 'HORAIN'  \n" +
                 "FROM MESA M lEFT JOIN  Agrupador A ON M.POSICION = A.Posicion AND A.Fecha='" + Variables.FechaStr + "' AND A.Suc_Id='" + Variables.Suc_Id + "'  AND  \n" +
@@ -57,6 +54,7 @@ public class RendArmado_Lista extends AppCompatActivity {
                 new int[]{R.id.text1, R.id.text2, R.id.text3}, SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         dgvPersonalRendimiento.setAdapter(AdaptadorGrilla);
         dgvPersonalRendimiento.setNumColumns(4);
+
         dgvPersonalRendimiento.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -66,10 +64,29 @@ public class RendArmado_Lista extends AppCompatActivity {
                 Variables.Per_Dni = curPersonal.getString(curPersonal.getColumnIndex("DNI"));
                 Variables.Agru_Id = curPersonal.getInt(curPersonal.getColumnIndex("AGRUID"));
                 Variables.HoraIngreso = curPersonal.getString(curPersonal.getColumnIndex("HORAIN"));
-                registerForContextMenu(dgvPersonalRendimiento);
+                Intent ActividadRegistrar = new Intent(RendArmado_Lista.this, RendArmado_Registro.class);
+                startActivity(ActividadRegistrar);
             }
         });
 
+        dgvPersonalRendimiento.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Cursor curPersonal = (Cursor) parent.getItemAtPosition(position);
+                Variables.Per_Ubicacion = curPersonal.getInt(curPersonal.getColumnIndex("_id"));
+                Variables.Per_Nombres = curPersonal.getString(curPersonal.getColumnIndex("PER"));
+                Variables.Per_Dni = curPersonal.getString(curPersonal.getColumnIndex("DNI"));
+                Variables.Agru_Id = curPersonal.getInt(curPersonal.getColumnIndex("AGRUID"));
+                Variables.HoraIngreso = curPersonal.getString(curPersonal.getColumnIndex("HORAIN"));
+                registerForContextMenu(dgvPersonalRendimiento);
+
+
+                return false;
+            }
+        })
+        ;
 
 
         imbConfigurar.setOnClickListener(new View.OnClickListener() {
@@ -106,6 +123,14 @@ public class RendArmado_Lista extends AppCompatActivity {
         //ASIGNACION DE VARIABLES A ELEMENTOS DEL LAYOUT
         imbRegresar = (ImageButton) findViewById(R.id.imbRegresar);
         imbConfigurar = (ImageButton) findViewById(R.id.imbConfigurar);
+    }
+    public void MostrarVariables()
+    {
+        lblEmpresa.setText(Variables.Emp_Abrev);
+        lblSucursal.setText(Variables.Suc_Descripcion);
+        lblLinea.setText(Variables.Lin_Descripcion);
+        lblLado.setText("LADO: " + Variables.Lin_Lado);
+        edtFecha.setText(Variables.FechaStr);
     }
 
     @Override
